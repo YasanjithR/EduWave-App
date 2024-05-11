6b8a3c1c0fc8d086e2b5185d2a2aa937735be593
RxshiA <rashiamarasiri@gmail.com>
2024-05-11 20:32:46 +0530

    S001 - Full Structure changes and adding hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './index.css';
-import {BrowserRouter, Routes,Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import HomeStudent from './pages/Home-Student';
 import ApproveCourseTablePage from './pages/ApproveCourseTablePage';
@@ -8,28 +7,27 @@ import EnrolledCoursesPage from './pages/EnrolledCourses';
 import CourseDetailsPage from './pages/CourseDetailsPage';
 import PaymentPage from './pages/PaymentPage';
 import CourseProgressTrackerPage from './pages/ProgressPage';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <div className='pages'>
-          
+
           <Routes>
-            <Route path="/" element ={<Home/>}/>
-            <Route path="/home-student" element ={<HomeStudent/>}/>
-            <Route path="/home-admin" element ={<ApproveCourseTablePage/>}/>
-            <Route path="/enrolled" element ={<EnrolledCoursesPage/>}/>
-            <Route path="/CourseDetails" element ={<CourseDetailsPage/>}/>
-            <Route path="/payment" element ={<PaymentPage/>}/>
-            <Route path="/progress" element ={<CourseProgressTrackerPage/>}/>
-            
-
-
+            <Route path="/" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/home-student" element={<HomeStudent />} />
+            <Route path="/home-admin" element={<ApproveCourseTablePage />} />
+            <Route path="/enrolled" element={<EnrolledCoursesPage />} />
+            <Route path="/CourseDetails" element={<CourseDetailsPage />} />
+            <Route path="/payment" element={<PaymentPage />} />
+            <Route path="/progress" element={<CourseProgressTrackerPage />} />
           </Routes>
-          </div> 
-      
-      
+
+        </div>
       </BrowserRouter>
     </div>
   );
